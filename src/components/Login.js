5b10d7169fd6d42c9d9739d2f69c438be01f0e2c
Jan Söderberg <jan.soderberg24@gmail.com>
2021-05-27 08:47:28 +0300

    Improve login and signup page styling. #3

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,7 +21,26 @@ const Login = () => {
     }
 
     const login = async (e) => {
+
         e.preventDefault()
+
+        let missingFields = false
+        if(username === '')
+        {
+            document.getElementById('username').style.border = '1px solid #EF4444'
+            missingFields = true
+        }
+        if(password === '')
+        {
+            document.getElementById('password').style.border = '1px solid #EF4444'
+            missingFields = true
+        }
+
+        if(missingFields)
+        {
+            setError('Missing fields.')
+            return
+        }
 
         const token = await applicationService.login(username,password) 
         if(token === null){
@@ -35,21 +54,22 @@ const Login = () => {
 
     return (
     <div class="flex flex-col justify-center items-center">
-        <div class="bg-white rounded border-gray-400 m-4 p-5 w-10/12 md:w-1/4 h-60 items-center flex flex-col justify-center">
-            <form onSubmit={login}>
+        <div class="bg-white rounded border-gray-400 m-4 p-5 w-10/12 md:w-1/4 h-80 items-center flex flex-col justify-center">
+            <h2 class="text-3xl pt-5">Log in</h2>
+            <form class="pt-5" onSubmit={login}>
                 <label>
                     <p>Username</p>
-                    <input type="text" class="border-2" onChange={e => setUsername(e.target.value)}></input>
+                    <input id="username" type="text" class="border-2" onChange={e => setUsername(e.target.value)}></input>
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" class="border-2" onChange={e => setPassword(e.target.value)}></input>
+                    <input id="password" type="password" class="border-2" onChange={e => setPassword(e.target.value)}></input>
                 </label>
                 <div>
                     <button class="bg-blue-600 text-base text-white mt-2 p-2 rounded w-40" type="submit">Log in</button>
                 </div>
 
-                <div>
+                <div class="pt-2">
                     <Link to="/signup" class="text-blue-400">Sign up</Link>
                 </div>
             </form>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import userService from '../services/user'
 import InfoBox from './InfoBox'
 
@@ -16,6 +17,29 @@ const SignUp = () => {
     const signup = (e) => {
 
         e.preventDefault()
+
+        let missingFields = false
+        if(username === '')
+        {
+            document.getElementById('username').style.border = '1px solid #EF4444'
+            missingFields = true
+        }
+        if(password === '')
+        {
+            document.getElementById('password').style.border = '1px solid #EF4444'
+            missingFields = true
+        }
+        if(confirmPassword === '')
+        {
+            document.getElementById('confirm-password').style.border = '1px solid #EF4444'
+            missingFields = true
+        }
+
+        if(missingFields)
+        {
+            setError('Missing fields.')
+            return
+        }
 
         // This fixes a warning to prevent race conditions.
         const f = async () => {
@@ -47,19 +71,20 @@ const SignUp = () => {
 
     return (
     <div class="flex flex-col justify-center items-center">
-        <div class="bg-white rounded border-gray-400 m-4 p-5 w-10/12 md:w-1/4 h-60 items-center flex flex-col justify-center">
-            <form onSubmit={signup}>
+        <div class="bg-white rounded border-gray-400 m-4 p-5 w-10/12 md:w-1/4 h-90 items-center flex flex-col justify-center">
+            <h2 class="text-3xl pt-5">Sign up</h2>
+            <form class="pt-5" onSubmit={signup}>
                     <p>Username:</p>
-                    <input type="text" class="border-2" onChange={e => setUsername(e.target.value)}></input>
+                    <input id="username" type="text" class="border-2" onChange={e => setUsername(e.target.value)}></input>
                     <p>Password:</p>
-                    <input type="password" class="border-2" onChange={e => setPassword(e.target.value)}></input>
+                    <input id="password" type="password" class="border-2" onChange={e => setPassword(e.target.value)}></input>
                     <p>Confirm password:</p>
-                    <input type="password" class="border-2" onChange={e => setConfirmPassword(e.target.value)}></input>
-                <div>
-                    <a href="/" class="text-blue-400"> Already have an account?</a>
-                </div>
+                    <input id="confirm-password" type="password" class="border-2" onChange={e => setConfirmPassword(e.target.value)}></input>
                 <div>
                     <button class="bg-blue-600 text-base text-white mt-2 p-2 rounded w-40" type="submit">Sign up</button>
+                </div>
+                <div class="pt-2">
+                    <Link to="/" class="text-blue-400"> Already have an account?</Link>
                 </div>
             </form>
         </div>
