f8c48b248cbb89937426d7079edfe283886fda35
Jan Söderberg <jan.soderberg24@gmail.com>
2021-05-27 13:49:56 +0300

    Make info messages wider on smaller screens. #3

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -6,7 +6,7 @@ const InfoBox = ({message, type}) => {
     const [style, setStyle] = useState(type === 'error' ? 'border-2 text-red-600 bg-red-200 border-red-400 ' : 'border-2 text-green-600 bg-green-200 border-green-400 ')
 
     return (
-        <div class={`flex justify-center w-1/4 ${style} mb-3`}>
+        <div class={`flex justify-center w-10/12 ${style} mb-3`}>
             <p>{message}</p>
         </div>
     )
