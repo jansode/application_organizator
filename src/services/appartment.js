ce77a4387c97d15fd6c54b99954cb554c0629bde
Jan Söderberg <jan.soderberg24@gmail.com>
2021-05-29 09:26:28 +0300

    Add appartment requests. #4

---
+++ src/services/appartment.js
@@ -0,0 +1,104 @@
+import axios from 'axios'
+
+const baseUrl = host + '/api'
+
+let host = ''
+if(process.env.NODE_ENV === 'development')
+{
+    host = 'http://localhost:3001'
+}
+
+const getUserAppartments = async () => {
+
+    const token = localStorage.getItem('token')
+    if(token === null)
+    {
+        return [] 
+    }
+
+    const config = {
+        headers : { 'Authorization' : `Bearer ${token}` , 'Content-Type' : 'application/json'}
+    }
+
+    const response = await axios.get(baseUrl+'/appartments', config)
+    return response.data
+}
+
+const getUserAppartment = async (appartmentId) => {
+
+    const token = localStorage.getItem('token')
+    if(token === null)
+    {
+        return null 
+    }
+
+    const config = {
+        headers : { 'Authorization' : `Bearer ${token}` , 'Content-Type' : 'application/json'}
+    }
+
+    const response = await axios.get(baseUrl+`/appartment/${appartmentId}`, config)
+    return response.data
+}
+
+const updateUserAppartment = async (appartmentId, data) => {
+
+    const token = localStorage.getItem('token')
+    if(token === null)
+    {
+        return null 
+    }
+
+    const config = {
+        headers : { 'Authorization' : `Bearer ${token}` , 'Content-Type' : 'application/json'}
+    }
+
+    const response = await axios.put(baseUrl+`/appartment/${appartmentId}`, data, config)
+    return response.data
+}
+
+const deleteUserAppartment = async (appartmentId) => {
+
+    const token = localStorage.getItem('token')
+    if(token === null)
+    {
+        return null 
+    }
+
+    const config = {
+        headers : { 'Authorization' : `Bearer ${token}` , 'Content-Type' : 'application/json'}
+    }
+
+    const response = await axios.delete(baseUrl+`/appartments/${appartmentId}`, config)
+    return response.data
+}
+
+const createUserAppartment = async (appartment) => {
+
+    const token = localStorage.getItem('token')
+    if(token === null)
+    {
+        return null 
+    }
+
+    const config = {
+        headers : { 'Authorization' : `Bearer ${token}` , 'Content-Type' : 'application/json'}
+    }
+
+    const data = {
+        ...appartment,
+        user: token.user 
+    }
+
+    const response = await axios.post(baseUrl+'/appartments/', data, config)
+    return response.data
+}
+
+const appartmentService = {
+    getUserAppartments, 
+    getUserAppartment, 
+    updateUserAppartment, 
+    deleteUserAppartment, 
+    createUserAppartment
+}
+
+export default appartmentService 
